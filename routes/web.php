80d9893874b65a336d8f00df10e1a6f9c838f82f
Jeca603 <jecajoycalope@gmail.com>
2021-08-23 06:33:06 +0800

    Printed [1, 2, 3, 4, 5, 6, 7, 8, 9]

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route:: get ('/product', function(){
-    	$num1 =3;
-    	$num2 =2;
-    	$result =$num1 * $num2;
-    	return " The product of ".$num1." and ".$num2." is ".$result;
+Route:: get ('/numbers', function(){
+
+    	return "[1, 2, 3, 4, 5, 6, 7, 8, 9]";
     }); 
